866c8c6d5b8bfa1181e237966dea20498e82f4e3
Nico <nicoargiz@icloud.com>
2018-08-21 15:10:29 -0300

    Cambio contraste contact-section. Agrego efectos JS.

--- js/index.js
+++ js/index.js
@@ -1,44 +1,77 @@
 $(document).ready(function () {
-  // SCROLL NOSOTROS SECTION
+  // VARIABLES SCROLLTOP
   var nosotrosButtonNode = $('#nosotrosButton');
-  nosotrosButtonNode.click(scrollingNosotros);
+  var estilosButtonNode = $('.estilosButton');
+  var contactButtonNode = $('#contactButton');
 
+  //VARIABLES SCROLLREVEAL
+  var slideRight2 = {
+    distance: '50%',
+    origin: 'left',
+    duration: 1000,
+    opacity: 0,
+    delay: 500,
+  };
+  var slideLeft1 = {
+    distance: '50%',
+    origin: 'right',
+    duration: 2000,
+    opacity: 0,
+    delay: 800,
+  };
+  var slideLeft2 = {
+    distance: '50%',
+    origin: 'right',
+    duration: 2000,
+    opacity: 0,
+    delay: 1500,
+  };
+  var slideUp2 = {
+    distance: '50%',
+    origin: 'bottom',
+    duration: 2000,
+    opacity: 0,
+    delay: 1500,
+  };
+
+  //LLAMADAS FUNCIONES ON-CLICK
+  nosotrosButtonNode.click(scrollingNosotros);
+  estilosButtonNode.click(scrollingEstilos);
+  contactButtonNode.click(scrollingContact);
+
+  //LLAMADAS SCROLLREVEAL
+  ScrollReveal({ reset: false }).reveal('.titles', slideRight2);
+  ScrollReveal({ reset: false }).reveal('.contact-title', slideRight2);
+  ScrollReveal({ reset: false }).reveal('.nosotros-text', slideLeft1);
+  ScrollReveal({ reset: false }).reveal('.contact-text', slideLeft1);
+  ScrollReveal({ reset: true }).reveal('#contact-form', slideUp2);
+  ScrollReveal({ reset: true }).reveal('#contact-button', slideUp2);
+  ScrollReveal({ reset: false }).reveal('#contact-info', slideLeft2);
+
+
+  //FUNCIONES
   function scrollingNosotros(section) {
     $('html, body').animate({
-        scrollTop: $('#nosotrosSection').offset().top - 50
-      },
+      scrollTop: $('#nosotrosSection').offset().top - 50
+    },
       1000
     );
   }
-
-  // SCROLL ESTILOS SECTION
-  var estilosButtonNode = $('.estilosButton');
-  estilosButtonNode.click(scrollingEstilos);
-
+  
   function scrollingEstilos(section) {
     $('html, body').animate({
-        scrollTop: $('#estilosSection').offset().top - 30
-      },
+      scrollTop: $('#estilosSection').offset().top - 30
+    },
       1000
     );
   }
-
-  // SCROLL CONTACT SECTION
-  var contactButtonNode = $('#contactButton');
-  contactButtonNode.click(scrollingContact);
 
   function scrollingContact(section) {
     $('html, body').animate({
-        scrollTop: $('#contactSection').offset().top - 70
-      },
+      scrollTop: $('#contactSection').offset().top - 70
+    },
       1000
     );
   }
-
-
-
-
-
-
 
 })
